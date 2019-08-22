92c277839b7df56c860e7a86f795bbcd4ba43f5f
aurelianito <aurelianito@github.com>
2019-08-22 04:11:59 +0000

    Calculate identifier for second diagonal

--- src/main/java/nq/Solver.java
+++ src/main/java/nq/Solver.java
@@ -4,4 +4,9 @@ public class Solver {
 	public static int diag1(int x, int y) {
 		return x+y;
 	}
+	
+	public static int diag2(int x, int y) {
+		return x-y;
+	}
+
 }
--- src/test/java/nq/SolverTest.java
+++ src/test/java/nq/SolverTest.java
@@ -10,5 +10,11 @@ public class SolverTest {
 	public void testDiag1() {
 		assertEquals(3, Solver.diag1(1, 2));
 	}
+	
+	@Test
+	public void testDiag2() {
+		assertEquals(-1, Solver.diag2(1, 2));
+	}
+
 
 }
