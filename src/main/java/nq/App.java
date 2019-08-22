2d561a324d831d2ce2c32ad9278f29403cea2752
aurelianito <aurelianito@github.com>
2019-08-22 04:35:09 +0000

    Receive command line params and invoke app via java -jar

--- src/main/java/nq/App.java
+++ src/main/java/nq/App.java
@@ -10,5 +10,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        for( String a: args) {
+        	System.out.println(a);
+        }
     }
 }
