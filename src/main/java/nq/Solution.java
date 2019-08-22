9f6f96c0a7a77493d040aecf2ccffd76140f7d8d
aurelianito <aurelianito@github.com>
2019-08-22 05:38:24 +0000

    Better output

--- src/main/java/nq/Solution.java
+++ src/main/java/nq/Solution.java
@@ -6,9 +6,6 @@ import java.util.stream.IntStream;
 public class Solution extends Exception {
 	/* Extends exception to ease backtracking bailout. I prefer to avoid ifs :) */
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Integer> vec;
 	
@@ -20,7 +17,7 @@ public class Solution extends Exception {
 		int size = vec.size();
 		IntStream.range(0, size).forEachOrdered(i -> {
 			IntStream.range(0, size).forEachOrdered( j -> {
-				System.out.print( vec.get(i) == j ? 'o' : '.');
+				System.out.print( vec.get(i) == j ? "o " : ". ");
 			});
 			System.out.println();
 		});
